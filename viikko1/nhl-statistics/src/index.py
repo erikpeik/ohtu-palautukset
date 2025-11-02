76a2b0ee67c0a3e6694f55400cfa95aeb80d6809
Erik Mende <erikpeik@outlook.com>
2025-11-02 18:52:24 +0200

    17. NHL-tilastot-ohjelman laajennus

--- viikko1/nhl-statistics/src/index.py
+++ viikko1/nhl-statistics/src/index.py
@@ -1,4 +1,4 @@
-from statistics_service import StatisticsService
+from statistics_service import SortBy, StatisticsService
 from player_reader import PlayerReader
 
 
@@ -8,17 +8,26 @@ def main():
             "https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt")
     )
 
-    philadelphia_flyers_players = stats.team("PHI")
-    top_scorers = stats.top(10)
-
-    print("Philadelphia Flyers:")
-    for player in philadelphia_flyers_players:
+    # järjestetään kaikkien tehopisteiden eli maalit+syötöt perusteella
+    print("Top point getters:")
+    for player in stats.top(5, SortBy.POINTS):
         print(player)
 
     print()
+    # metodi toimii samalla tavalla kuin yo. kutsu myös ilman toista parametria
+    for player in stats.top(5):
+        print(player)
 
-    print("Top point getters:")
-    for player in top_scorers:
+    print()
+    # järjestetään maalien perusteella
+    print("Top point goal scorers:")
+    for player in stats.top(5, SortBy.GOALS):
+        print(player)
+
+    print()
+    # järjestetään syöttöjen perusteella
+    print("Top by assists:")
+    for player in stats.top(5, SortBy.ASSISTS):
         print(player)
 
 
--- viikko1/nhl-statistics/src/tests/statistics_service_test.py
+++ viikko1/nhl-statistics/src/tests/statistics_service_test.py
@@ -1,5 +1,5 @@
 import unittest
-from statistics_service import StatisticsService
+from statistics_service import SortBy, StatisticsService
 from player import Player
 
 
@@ -43,3 +43,17 @@ class TestStatisticsService(unittest.TestCase):
         self.assertEqual(top_players[0].name, "Gretzky")  # 124
         self.assertEqual(top_players[1].name, "Lemieux")  # 99
         self.assertEqual(top_players[2].name, "Yzerman")  # 98
+
+    def test_top_goal_scorers(self):
+        top_players = self.stats.top(2, sort_by=SortBy.GOALS)
+        self.assertEqual(len(top_players), 3)
+        self.assertEqual(top_players[0].name, "Lemieux")  # 45
+        self.assertEqual(top_players[1].name, "Yzerman")  # 42
+        self.assertEqual(top_players[2].name, "Kurri")    # 37
+
+    def test_top_assist_scorers(self):
+        top_players = self.stats.top(2, sort_by=SortBy.ASSISTS)
+        self.assertEqual(len(top_players), 3)
+        self.assertEqual(top_players[0].name, "Gretzky")  # 89
+        self.assertEqual(top_players[1].name, "Yzerman")  # 56
+        self.assertEqual(top_players[2].name, "Lemieux")  # 54
