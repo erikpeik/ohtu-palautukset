1e010999d88f7563bf35afa5afc263bd703a2484
Erik Mende <erikpeik@outlook.com>
2025-11-10 19:24:07 +0200

    Viikko 2: 4. Pelaajalistan refaktorointi

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,28 +1,16 @@
-import requests
-from player import Player
+from player_reader import PlayerReader
+from player_stats import PlayerStats
 
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-    nationality = "FIN"
-
-
-    print(f"Players from {nationality}:")
-    print()
-
-    nationality_players = list(filter(lambda p: p.nationality == nationality, players))
-    nationality_players.sort(key=lambda p: p.goals + p.assists, reverse=True)
-
-    for player in nationality_players:
+    for player in players:
         print(player)
+
 
 if __name__ == "__main__":
     main()
---
+++ viikko2/nhl-reader/src/player_stats.py
@@ -0,0 +1,12 @@
+class PlayerStats:
+    def __init__(self, reader):
+        self._reader = reader
+        self._players = self._reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality):
+        nationality_players = list(
+            filter(lambda p: p.nationality == nationality, self._players))
+
+        nationality_players.sort(
+            key=lambda p: p.goals + p.assists, reverse=True)
+        return nationality_players
