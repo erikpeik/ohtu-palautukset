b008dd13301df41eb2b7afaeb79f099bee356eb6
Erik Mende <erikpeik@outlook.com>
2025-11-10 20:29:00 +0200

    Viikko 2: 5. Graafinen pelaajalista

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,6 +1,31 @@
 from player_reader import PlayerReader
 from player_stats import PlayerStats
 
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+
+def render_players_table(players):
+    table = Table(title="Pelaajat")
+
+    table.add_column("Released", style="cyan", no_wrap=True)
+    table.add_column("teams", style="magenta")
+    table.add_column("goals", justify="right", style="green")
+    table.add_column("assists", justify="right", style="green")
+    table.add_column("points", justify="right", style="green")
+
+    for player in players:
+        table.add_row(
+            player.name,
+            player.team,
+            str(player.goals),
+            str(player.assists),
+            str(player.points),
+        )
+    console.print(table)
+
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
@@ -10,6 +35,8 @@ def main():
 
     for player in players:
         print(player)
+
+    render_players_table(players)
 
 
 if __name__ == "__main__":
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -4,8 +4,9 @@ class Player:
         self.nationality = dict['nationality']
         self.assists = dict['assists']
         self.goals = dict['goals']
+        self.points = dict['goals'] + dict['assists']
         self.team = dict['team']
         self.games = dict['games']
 
     def __str__(self):
-        return f"{self.name:<25} {self.team:<15} {self.goals} + {self.assists} = {self.goals + self.assists}"
+        return f"{self.name:<25} {self.team:<15} {self.goals} + {self.assists} = {self.points}"
