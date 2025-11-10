06e5e1140296cc9997128ab2494631502487dd89
Erik-Peik Mende <erik-peik.mende@tietoevry.com>
2025-11-10 17:22:10 +0200

    Viikko 2: 2. Riippuvuuksien hyödyntäminen: Pelaajalista

---
+++ viikko2/nhl-reader/src/index.py
@@ -0,0 +1,26 @@
+import requests
+from player import Player
+
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+
+    nationality = "FIN"
+
+
+    print(f"Players from {nationality}:")
+    print()
+
+    for player in players:
+        if player.nationality == nationality:
+            print(player)
+
+if __name__ == "__main__":
+    main()
